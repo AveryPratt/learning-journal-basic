cead77eb1d1b035229210e38927ea1043467a14c
Avery Pratt <apratt91@gmail.com>
2016-12-21 08:36:32 -0800

    jinja templates are setup but incomplete

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,61 +1,38 @@
-# from pyramid.view import view_config
-
-# def home_page(request):
-#     return Response("This is my first view!")
-
-
-# @view_config(route_name='home', renderer='/')
-# def my_view(request):
-#     return {'project': 'learning_journal_basic'}
-
-
-# @view_config(route_name='detail', renderer='/journal/{id:\d+}')
-# def my_view(request):
-#     return {'project': 'learning_journal_basic'}
-
-
-# @view_config(route_name='create', renderer='/journal/new-entry')
-# def my_view(request):
-#     return {'project': 'learning_journal_basic'}
-
-
-# @view_config(route_name='update', renderer='/journal/{id:\d+}/edit-entry')
-# def my_view(request):
-#     return {'project': 'learning_journal_basic'}
-
-
 import os
 import io
 from pyramid.view import view_config
 
 
+ENTRIES = [
+    {"id": 1, "title": "Entry 1", "date": "12/19/16", "body": "this is the body for entry 1"},
+    {"id": 2, "title": "Entry 2", "date": "12/20/16", "body": "this is the body for entry 2"},
+    {"id": 3, "title": "Entry 3", "date": "12/21/16", "body": "this is the body for entry 3"}
+]
+
+
 @view_config(route_name="home", renderer="templates/averypratt-mockups/home.jinja2")
 def home(request):
     return {
-        "title": "Entry 11",
-        "date": "12/19/16",
-        "body": "body placeholder"
+        "entries": ENTRIES
     }
 
 
 @view_config(route_name="detail", renderer="templates/averypratt-mockups/detail.jinja2")
 def detail(request):
     return {
-        "title": "Entry 11",
-        "body": "body placeholder",
-        "date": "12/19/16"
+        "entries": ENTRIES
     }
 
 
 @view_config(route_name="create", renderer="templates/averypratt-mockups/create_form.jinja2")
 def create(request):
-    return {}
+    return {
+        "entries": ENTRIES
+    }
 
 
 @view_config(route_name="update", renderer="templates/averypratt-mockups/edit_form.jinja2")
 def update(request):
     return {
-        "title": "Entry 11",
-        "body": "body placeholder",
-        "date": "12/19/16"
+        "entries": ENTRIES
     }
