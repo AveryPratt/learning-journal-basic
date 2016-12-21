e0dd8506448eae817c1117458f856f321c47efc3
Avery Pratt <apratt91@gmail.com>
2016-12-20 17:46:45 -0800

    routes and views are set up

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,24 +1,52 @@
+# from pyramid.view import view_config
+
+# def home_page(request):
+#     return Response("This is my first view!")
+
+
+# @view_config(route_name='home', renderer='/')
+# def my_view(request):
+#     return {'project': 'learning_journal_basic'}
+
+
+# @view_config(route_name='detail', renderer='/journal/{id:\d+}')
+# def my_view(request):
+#     return {'project': 'learning_journal_basic'}
+
+
+# @view_config(route_name='create', renderer='/journal/new-entry')
+# def my_view(request):
+#     return {'project': 'learning_journal_basic'}
+
+
+# @view_config(route_name='update', renderer='/journal/{id:\d+}/edit-entry')
+# def my_view(request):
+#     return {'project': 'learning_journal_basic'}
+
+
+import io
 from pyramid.response import Response
 
-def home_page(request):
-    return Response("This is my first view!")
+
+def home(request):
+    pass
 
 
-@view_config(route_name='home', renderer='/')
-def my_view(request):
-    return {'project': 'learning_journal_basic'}
+def detail(request):
+    pass
 
 
-@view_config(route_name='detail', renderer='/journal/{id:\d+}')
-def my_view(request):
-    return {'project': 'learning_journal_basic'}
+def create(request):
+    pass
 
 
-@view_config(route_name='create', renderer='/journal/new-entry')
-def my_view(request):
-    return {'project': 'learning_journal_basic'}
+def update(request):
+    pass
 
 
-@view_config(route_name='update', renderer='/journal/{id:\d+}/edit-entry')
-def my_view(request):
-    return {'project': 'learning_journal_basic'}
+def includeme(config):
+    config.add_view(home, route_name='home')
+    config.add_view(detail, route_name='detail')
+    config.add_view(create, route_name='create')
+    config.add_view(update, route_name='update')
+
